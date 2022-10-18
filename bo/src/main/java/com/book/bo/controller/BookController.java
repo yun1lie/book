a74ffa69eb8db2774b61636c349c706f4465de0d
rotten <1132264073@qq.com>
2022-10-18 17:38:57 +0800

    infobook

---
+++ bo/src/main/java/com/book/bo/controller/BookController.java
@@ -0,0 +1,2 @@
+package com.book.bo.controller;public class BookController {
+}
---
+++ bo/src/main/java/com/book/bo/mapper/InfBookMapper.java
@@ -0,0 +1,11 @@
+package com.book.bo.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface InfBook {
+    //添加书籍信息
+
+    @Insert("")
+}
